495e2cf8d0231d3a411f0003363f075dc2d6feee
dingyl <dingyunlong1996@hotmail.com>
2017-12-25 21:30:52 +0800

    v1.1

---
+++ Ch1/src/Ex1_1_3.java
@@ -0,0 +1,10 @@
+public class Ex1_1_3 {
+    public static int a=1,b=2,c=3;
+    public static void main(String[] args){
+        if(a==b&&a==c){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
